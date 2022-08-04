e473d02ee546351c1477d9f9a2fa620c44ad66e8
sun-wukong <jasonc.scoon@gmail.com>
2022-08-04 17:25:22 -0400

    Initial branch commit

---
+++ sleeve-py/src/lib/IaC/cfn-s3-factory.py
@@ -0,0 +1,15 @@
+import boto3
+import boto3.session
+import troposphere
+from troposphere.template_generator import TemplateGenerator
+import troposphere.s3 as s3
+import minio
+import re
+
+
+def get_s3_details():
+    pass
+
+
+def build_bucket_template(template=troposphere.Template()):
+    pass
