85ade96e56896d45c8592e218114cba2f8d0984c
sun-wukong <jasonc.scoon@gmail.com>
2022-08-16 17:36:40 -0400

    Updated client script, check JMESPath queries to make sure

--- sleeve-py/src/lib/IaC/Resource/Ec2.py
+++ sleeve-py/src/lib/IaC/Resource/Ec2.py
@@ -3,9 +3,11 @@ import json
 import boto3
 import boto3.session
 
-def filter_by(key="", operand="==", value=""):
-    key_filter = jmespath.compile("?{}{}'{}'".format(key, operand, property))
-    return key_filter
+def filter_by(key="", operand="==", value="", options={}):
+    if options != None:
+        return jmespath.compile("?{}{}'{}'".format(options["key"], options["operand"], options["value"]))
+    else:
+        return jmespath.compile("?{}{}'{}'".format(key, operand, value))
 
 
 def get_security_groups(session=boto3.session.Session(), filters=[]):
@@ -13,7 +15,24 @@ def get_security_groups(session=boto3.session.Session(), filters=[]):
     secgroups = ec2.describe_security_groups()
     if len(filters) == 0:
         return json.loads(secgroups)["SecurityGroups"]
+    elif type(filters[0]) == str:
+        if len(filters == 3):
+            return [json.loads(result) for result in jmespath.search(filter_by(
+                key=filters[0],
+                operand=filters[1],
+                value=filters[-1]
+        ),secgroups)]
+        else:
+            return [json.loads(result) for result in jmespath.search(filter_by(
+                key=filters[0],
+                value=filters[-1]
+            ),secgroups)]
     else:
+        results = {}
+        for item in filters:
+            results["by_{}".format(item["key"])] = [sg for sg in jmespath.search(filter_by(
+                options=item
+            ),secgroups)]
         pass
 
 
